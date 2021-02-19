ae18c9ac49b44428a3bde8efe0d5823ebb2a1778
Carlos Vadillo <cavarod@gmail.com>
2021-02-19 15:35:19 -0800

    changed the html to incorporat the questions, began modifying the javascript to pull the information from the DOM into variables.

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-// Assignment code here
+// Set-up Variables from the DOM
 
+// Password Composition and Elements
+var lengthEl = document.getElementById("length");
+var upperCaseEl = document.getElementById("upperCase");
+var lowerCaseEl = document.getElementById("lowerCase");
+var numberCharEl = document.getElementById("numberChar");
+var specialCharEl = document.getElementById("specialChar");
 
-// Get references to the #generate element
+// Button Variable
 var generateBtn = document.querySelector("#generate");
+
+var getPasswordParameters = function () {
+	var hasLength = lengthEl.value;
+	var hasUpper = upperCaseEl.checked;
+	var hasLower = lowerCaseEl.checked;
+	var hasNumber = numberCharEl.checked;
+	var hasSpecial = specialCharEl.checked;
+
+
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	// Set password length and complexity
 
-  passwordText.value = password;
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-}
+	passwordText.value = password;
+
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPasswordParameters);
